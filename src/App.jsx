1fb857cc53b0ef32d32552aa836d31610df1af9a
PIYUSH1525 <negipiyush125@gmail.com>
2025-04-23 22:15:29 +0530

    Interface Done LLM Model ADDED

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,40 @@ import "./app.css"
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const[messages,setMessages] = useState([{
+    role: "model",
+    content: "Hello, how can I help you?"
+  },{
+    role: "user",
+    content: "Hello , How are you ?"
+  }, {
+    role: "model",
+    content: "What kind of job are you looking for?"
+  }, {
+    role: "user",
+    content: "I am looking for a software engineer position"
+  }, {
+    role: "model",
+    content: "My Name Is GPT"
+  }
+  ])
+    
   return (
     <main>
       <section>
         <div className='Conv-Area'>
+
+          <div className="messages">
+            {
+              messages.map((message, index) => {
+                return (
+                  <div className={`message ${message.role}`} key={index}>
+                    {message.content}
+                  </div>
+                );
+              })
+            }
+          </div>
 
 
           <div className='user-Area'>
